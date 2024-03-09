fa6180199d66c55e15af3c683d8dba5ef930a97c
mario <mkalp@wireless-10-105-213-71.umd.edu>
2024-03-09 03:34:55 -0500

    added the file

---
+++ jk-scroll-mode.el
@@ -0,0 +1,87 @@
+(defun jk-scroll-half (direction)
+  "copied from https://www.emacswiki.org/emacs/HalfScrolling"
+  (let ((opos (cdr (nth 6 (posn-at-point)))))
+    ;; opos = original position line relative to window
+    (move-to-window-line nil)  ;; Move cursor to middle line
+      (if direction
+          (recenter-top-bottom -1)  ;; Current line becomes last
+        (recenter-top-bottom 0))  ;; Current line becomes first
+      (move-to-window-line opos)) ;; Restore cursor/point position
+  )  
+
+(defun jk-scroll-half-page-down ()
+  (interactive)
+  (jk-scroll-half nil))
+
+(defun jk-scroll-half-page-up ()
+  (interactive)
+  (jk-scroll-half t))
+
+(define-minor-mode jk-scroll-mode
+  "Toggles global jk-scroll-mode" ;; i want buffer local though
+  :init-value t ;; initial value, nil for disabled by default,
+  ;;t for enabled
+  :global t ;; maybe changing this is what i want for buffer loc
+  ;; :group 'jk-scroll ;; i have no idea what this is
+  :lighter " jk-scroll" ;; thing displayed at bottom for mode
+  :keymap (define-keymap
+  "j" #'next-line
+  "k" #'previous-line
+  "l" #'left-char
+  ";" #'right-char
+  "i" #'jk-scroll-mode ;; used to go back to typing
+  "C-l" #'left-word
+  "C-;" #'right-word ;; why doesnt this one work?
+  "C-j" #'jk-scroll-half-page-down
+  "C-k" #'jk-scroll-half-page-up
+  ;; make the rest of the keys nil so i dont try to type while
+  ;; in this mode
+  "q" #'ignore ;; is this possible to do using regex instead?
+  "w" #'ignore
+  "e" #'ignore
+  "r" #'ignore
+  "t" #'ignore
+  "y" #'ignore
+  "u" #'ignore
+  "o" #'ignore
+  "p" #'ignore
+  "[" #'ignore
+  "]" #'ignore
+  "\\" #'ignore ;; this one may be weird
+  "a" #'ignore
+  "s" #'ignore
+  "d" #'ignore
+  "f" #'ignore
+  "g" #'ignore
+  "h" #'ignore
+  "'" #'ignore
+  "z" #'ignore
+  "x" #'ignore
+  "c" #'ignore
+  "v" #'ignore
+  "b" #'ignore
+  "n" #'ignore
+  "m" #'ignore
+  "," #'ignore
+  "." #'ignore
+  "/" #'ignore
+  "`" #'ignore
+  "1" #'ignore
+  "2" #'ignore
+  "3" #'ignore
+  "4" #'ignore
+  "5" #'ignore
+  "6" #'ignore
+  "7" #'ignore
+  "8" #'ignore
+  "9" #'ignore
+  "0" #'ignore
+  "-" #'ignore
+  "=" #'ignore
+  ) 
+  )
+(add-hook 'jk-scroll-mode-on-hook (lambda() (interactive)
+									(message "jk-scroll on!")))
+(add-hook 'jk-scroll-mode-off-hook (lambda () (interactive)
+									 (message "jk-scroll off!")))
+(global-set-key (kbd "C-c n") #'jk-scroll-mode)
